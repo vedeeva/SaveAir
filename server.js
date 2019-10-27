a9fe610ba641faf5c99ab58735305c24bbf99602
dwolfson486 <dwolfson486@gmail.com>
2019-10-26 18:30:19 -0700

    Updated Map API

--- server.js
+++ server.js
@@ -21,25 +21,3 @@ const port = process.env.PORT || 3001;
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// Get's the data from openAQ
-function getCities() {
-  fetch('https://api.openaq.org/v1/latest?parameter=co&has_geo').then(response => {
-    return response.json();
-  }).then(data => {
-    var cityData=[]
-    var length = Object.keys(data.results).length
-    for (var i = 0; i < length; i++) {
-      var city = {
-        "lat" : data.results[i].coordinates.latitude,
-        "lng" : data.results[i].coordinates.longitude,
-        "count" : data.results[i].measurements[0].value,
-      }
-      cityData.push(city);
-    }
-  return cityData;
-  }).catch(err => {
-    console.log(err.error);
-  });
-}
-var data = getCities();
